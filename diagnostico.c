daa513c6b3869bb999a8bb716a7ced9a62fe3ec3
Jorge Gálvez <jorgej.galvez@alumnos.udg.mx>
2020-09-21 10:39:32 -0500

    Se agrego un switch/case y la funcion para capturar los valores,  hacer la suma y calcular el promedio

--- diagnostico.c
+++ diagnostico.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+int indice;
+
+void capturarValores(){
+    int valores [5];
+    for ( indice = 0; indice < 5; indice++)
+    {
+      printf("Ingresa el valor %i\n",indice+1);
+      scanf("%i", &valores[indice]); 
+    }
+ 
+    for ( indice = 0; indice < 5; indice++)
+      printf("El numero en la posicion %i es %i \n", indice, valores[indice]);
+ 
+      int suma = 0;
+      for (indice = 0; indice < 5; indice++)
+      suma += valores[indice];
+      printf("La suma de los 5 numeros es %d \n", suma);
+      float promedio = suma / 5.0; 
+      printf("El promedio de los 5 numeros es %.2f \n", promedio);
+}
+
 int main(){
 
     int opcion;
@@ -11,7 +32,17 @@ int main(){
 		printf("0. Capturar enteros\n");
 			
 		scanf ("%d", &opcion);
+
+        switch (opcion)
+        {
+        case 1:
+            capturarValores();
+            break;
+        
+        default:
+            break;
+        }
     }while (opcion != 0);
-    
+
 return 0;
 }
